97114883fceca4acedd32844b9ded8af98f444ec
Beatriz JB <bjbilrero@gmail.com>
2021-06-16 12:28:19 +0100

    updating repo

--- js/Ball.js
+++ js/Ball.js
@@ -2,15 +2,9 @@ class Ball {
   constructor() {
     this.x = 395;
     this.y = 250;
-<<<<<<< HEAD
     this.vx = 5;
     this.vy = 5;
     this.radius = 10;
-=======
-    this.vx = 4;
-    this.vy = 4;
-    this.radius = 8;
->>>>>>> 0221b43a168c5578c51b3a42b290828e43236723
     this.color = "white";
   }
 
